0e99b86be62c3ccaefb3863d6a76ca84056ac37e
rosspa <rosspa@rose-hulman.edu>
2016-05-26 20:07:16 -0400

    added sudocode

--- src/out_of_order_execution_iterator/OutOfOrderExecutionIterator.java
+++ src/out_of_order_execution_iterator/OutOfOrderExecutionIterator.java
@@ -1,19 +1,36 @@
 package out_of_order_execution_iterator;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class OutOfOrderExecutionIterator<T> {
 	private int maxNumberOfThreads;
-	private ArrayList<T> collection;
+	private List<T> collection;
 
-	public OutOfOrderExecutionIterator(int maxNumberOfThreads, ArrayList<T> collection) {
+	public OutOfOrderExecutionIterator(int maxNumberOfThreads, List<T> collection) {
 		this.maxNumberOfThreads = maxNumberOfThreads;
 		this.collection = collection;
 	}
 
 	public void forEachExecuteOutOfOrder(Executer<T> execute, Method<T> callback) {
-		 for (int i = 0; i < this.collection.size(); i++) {
-//		for (int i = this.collection.size() - 1; i >= 0; i--) {
+		// TODO mess with thread pools
+		// use synchroized int index to assign tasks and keep track if done?
+		
+		// while still stuff do to DO
+		// if not at max threads DO
+		// create new thread with task
+		// IN THREAD do direct computation
+		// IN THREAD spin up new thread
+		// END
+		// do direct computation
+		// add callback to heap ordered by index
+		// while heapIndexVal = nextIndexToCallback DO
+		// 
+		// END
+		// END
+		
+		// direct computation
+		for (int i = 0; i < this.collection.size(); i++) {
+			// for (int i = this.collection.size() - 1; i >= 0; i--) {
 			T obj = this.collection.get(i);
 			T result = execute.execute(obj);
 			callback.execute(result);
